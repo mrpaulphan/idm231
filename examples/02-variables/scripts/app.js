d89c200424bcba518f8334bdde813a6e7e56e826
Paul Phan <paul@happycog.com>
2022-01-13 19:39:39 -0500

    Work in progress

---
+++ examples/02-variables/scripts/app.js
@@ -0,0 +1,55 @@
+// This is a single line comment
+/* Hey this is a multiline comment */
+console.log("This is a string");
+
+var principle = "Paul Phan";
+console.log(principle);
+console.log("principle");
+
+var teacher = "Jane Doe";
+var value1 = 10;
+var value2 = 3;
+var value3 = 4;
+var value4 = 9;
+// Math with integers
+var multiplyValue = value1 * value2;
+var addValue = value1 + value2;
+var minusValue = value1 - value2;
+var divideValue = value1 / value2;
+var multiplyValue = value1 * value2;
+
+console.log(multiplyValue);
+console.log(addValue);
+console.log(minusValue);
+console.log(divideValue);
+// console.log("20");
+
+var score = 20;
+var gameText = "Your score is";
+
+// Your score is20
+console.log(gameText + " " + score);
+
+var string = "some string here";
+var integer = 28;
+var floatNumber = 20.28;
+var boolean = true; // Can be true or false
+
+// Example of if statement
+var age = 17;
+var drinkingLimit = 21;
+var mexicoDrinkingLimit = 16;
+// Simple if else statement
+if (age > drinkingLimit) {
+  console.log("You are old enough");
+} else {
+  console.log("go to sleep");
+}
+
+// Example of boolean
+var doYouHaveADog = false;
+if (doYouHaveADog) {
+  console.log("Yes you have a dog");
+} else {
+  console.log("other");
+}
