921f69bc82c13d70ebd596739e263d122b246cd8
Paul Phan <paul@happycog.com>
2022-02-10 20:27:24 -0500

    Updates

--- examples/projects/01-toggle-darkmode/app.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.querySelector(".js-toggle");
-
-toggleButton.addEventListener("click", function () {
-  const body = document.querySelector("body");
-  body.classList.toggle("dark-mode");
-});
