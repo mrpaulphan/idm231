ef027cdffea5737728eed268271fb66ac7a1955e
Paul Phan <paul@happycog.com>
2022-02-24 20:00:12 -0500

    Final audios

--- examples/projects/07-soundboard/src/scripts/app.js
+++ examples/projects/07-soundboard/src/scripts/app.js
@@ -1,1 +1,15 @@
-// https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Using_Web_Audio_API
+// Array of buttons - [button1, button2, button3]
+const soundButtons = document.querySelectorAll(".js-sound-button");
+const audioPlayer = document.querySelector("#speakers");
+
+// Loop through all buttons
+soundButtons.forEach(function (individualButtonElement) {
+  // Get the value of the "data-audio-file" attribute on this button
+  const audioTrackSource = individualButtonElement.getAttribute("data-audio-file");
+  // Add click event listener to each button
+  individualButtonElement.addEventListener("click", function () {
+    // https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Using_Web_Audio_API
+    audioPlayer.src = `./src/sounds/${audioTrackSource}`
+    audioPlayer.play();
+  });
+});
